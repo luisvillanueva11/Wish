2e426466837ead44a000d6f9cddf057e6554b21b
Luis Villanueva <luisvillanueva11@gmail.com>
2022-12-24 12:44:25 +0100

    Vercel 1

--- wish/settings.py
+++ wish/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r2-84&&5*+fmx&zl%examz7kh56xrk0_1)+82=40tve!o_*(y9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -119,13 +119,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#    BASE_DIR / "static",
-#    '/var/www/static/',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- wish/urls.py
+++ wish/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('', views.index, name='index'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
